93f8740b338b7450db77d40b9dc452e871eb9209
Pedro Victor <pedro.luis.almeida.victor@protonmail.com>
2022-03-12 22:35:56 -0500

    indexing in db : abstract, url, title; init

--- webcrawler.py
+++ webcrawler.py
@@ -1,8 +1,9 @@
 import os
-import threading
+import re
 import requests
 from bs4 import BeautifulSoup
 import nltk
+import sqlite3
 
 # thread crawl
 # get a top world from a frequency list.
@@ -18,27 +19,78 @@ def random_word_selector():
 
     request(random_word)
 
-
+#for getting scholar.google.com search subdomains
 def request(current_word):
     r = requests.get('https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q=' + current_word +'&btnG=')
 
     if r.status_code == 200:
-        r.text
         soup = BeautifulSoup(r.text, 'html.parser')
         #print(soup.title)
         # Subdomains
         ## Cycles through all the h3 (where the articles are)
+        list_of_links = []
         for link in soup.find_all('h3'):
             ## Gets article link.
             print(link.a.get('href'))
+            index(link.a.get('href'))
 
         #print(soup.prettify())
     else:
         print(r.status_code)
 
 
-def index():
+def index(url_to_index):
+    connection = sqlite3.connect("indexer.db")
+    crsr = connection.cursor()
+    print("Connected to the db.")
 
+    if url_to_index.endswith(".pdf") or url_to_index.endswith(".pdf/"):
+        pass
+    else:
+        r = requests.get(url_to_index)
+
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, 'html.parser')
+            print("Title: " + soup.title.text)
+
+            #abstract_data1 = soup.find("meta", name="citation_abstract")
+            #print(abstract_data.get('content'))
+
+            abstract_data = soup.find("meta", property="og:description")
+
+            print(url_to_index)
+                
+            print("Abstract: " + abstract_data.get('content'))
+
+            authors_data = ""
+            for author in soup.find_all("dc.creator"):
+                authors_data = authors_data + ", " + author.get('content')
+                
+            print(authors_data)
+
+
+            sql_command = "INSERT INTO domains (website_url, abstract, url_title)\n VALUES ('" + url_to_index +  "', '" + abstract_data.get('content') + "', '" + soup.title.text + "')"
+
+            crsr.execute(sql_command)
+            connection.commit()
+
+            exit()
+
+
+        else:
+            print(r.status_code)
+
+        
+
+        sql_command = """SELECT * from domains"""
+        crsr.execute(sql_command)
+        connection.commit()
+
+
+
+    connection.close()
+    print("Connection closed.")
+    
 
 def test(word):
     request(word)
@@ -47,3 +99,7 @@ def test(word):
 test("Finasteride")
 
 
+# title
+# language : get from scholar search url.
+# main text : maybe search for asbtract in bs4; cleaned with nltk
+# author : search for author in bs4
